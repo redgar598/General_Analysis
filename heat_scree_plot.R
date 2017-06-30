768a7624e22c5a7ba64b5581ee3f11aa170e0549
Rachel Edgar <redgar598@gmail.com>
2017-06-30 10:44:22 -0700

    Create heat_scree_plot.R

---
+++ heat_scree_plot.R
@@ -0,0 +1,90 @@
+PCA_full<-princomp(BLBR_Beta[complete.cases(BLBR_Beta),]) # only using the 417619 complete rows for PCA
+## If you scale and center in this princomp, shit going to get crazy (PC0 will effectively be removed twice if you do). 
+# Someone will someday update this so it works with center and scale
+Loadings<-as.data.frame(unclass(PCA_full$loadings))
+vars <- PCA_full$sdev^2
+Importance<-vars/sum(vars)
+
+
+#Restructure meta
+meta$Barcode<-as.factor(meta$Barcode)
+
+meta_categorical <- meta[, c(4,5,7,9,13)]  # input column numbers in meta that contain categorical variables
+meta_continuous <- meta[, c(10,11,12)]  # input column numbers in meta that contain continuous variables
+colnames(meta_categorical) <- c("Barcode", "Section", "Tissue Type","Gender","Array Row")
+colnames(meta_continuous) <- c("Age", "PH", "Refrigeration Delay Hours")
+
+ord <- c(seq(1:sum(ncol(meta_categorical), ncol(meta_continuous))))
+# or ord<- c(1,4,5,2) etc if you want to specify the order of the rows in the plot
+
+# how far do you want the plot to go?
+PCs_to_view<-20
+
+heat_scree_plot<-function(Loadings, Importance){
+  adjust<-1-Importance[1]
+  pca_adjusted<-Importance[2:length(Importance)]/adjust
+  pca_df<-data.frame(adjusted_variance=pca_adjusted, PC=seq(1:length(pca_adjusted)))
+  
+  scree<-ggplot(pca_df[which(pca_df$PC<(PCs_to_view+1)),],aes(PC,adjusted_variance))+geom_bar(stat = "identity",color="black",fill="grey")+theme_bw()+
+        theme(axis.text = element_text(size =12),
+              axis.title = element_text(size =15),
+              plot.margin=unit(c(1.25,1.6,0.2,3),"cm"))+ylab("Adjusted Variance")+
+    scale_x_continuous(breaks = seq(1,PCs_to_view,1))
+  
+  
+  #### Heat
+  ## correlate meta with PCS
+  ## Run anova of each PC on each meta data variable
+
+
+ aov_PC_meta <- lapply(1:ncol(meta_categorical), function(covar) sapply(1:ncol(Loadings), 
+        function(PC) summary(aov(Loadings[, PC] ~ meta_categorical[, covar]))[[1]]$"Pr(>F)"[1]))
+  cor_PC_meta <- lapply(1:ncol(meta_continuous), function(covar) sapply(1:ncol(Loadings), 
+        function(PC) (cor.test(Loadings[, PC], as.numeric(meta_continuous[, 
+            covar]), alternative = "two.sided", method = "spearman", na.action = na.omit)$p.value)))
+ names(aov_PC_meta) <- colnames(meta_categorical)
+    names(cor_PC_meta) <- colnames(meta_continuous)
+    aov_PC_meta <- do.call(rbind, aov_PC_meta)
+    cor_PC_meta <- do.call(rbind, cor_PC_meta)
+    aov_PC_meta <- rbind(aov_PC_meta, cor_PC_meta)
+    aov_PC_meta <- as.data.frame(aov_PC_meta)
+  
+  #adjust
+  aov_PC_meta_adjust<-aov_PC_meta[,2:ncol(aov_PC_meta)]
+    
+  #reshape
+  avo<-aov_PC_meta_adjust[,1:PCs_to_view]
+  avo_heat_num<-apply(avo,2, as.numeric)
+  avo_heat<-as.data.frame(avo_heat_num)
+  avo_heat$meta<-rownames(avo)
+  avo_heat_melt<-melt(avo_heat, id=c("meta"))
+  
+  # cluster meta data
+  meta_var_order<-unique(avo_heat_melt$meta)[rev(ord)]
+  avo_heat_melt$meta <- factor(avo_heat_melt$meta, levels = meta_var_order)
+
+  # color if sig
+  avo_heat_melt$Pvalue<-sapply(1:nrow(avo_heat_melt), function(x) if(avo_heat_melt$value[x]<=0.001){"<=0.001"}else{
+    if(avo_heat_melt$value[x]<=0.01){"<=0.01"}else{
+      if(avo_heat_melt$value[x]<=0.05){"<=0.05"}else{">0.05"}}})
+  
+ levels(avo_heat_melt$variable)<-sapply(1:PCs_to_view, function(x) paste("PC",x, sep="" ))
+ 
+  heat<-ggplot(avo_heat_melt, aes(variable,meta, fill = Pvalue)) +
+  geom_tile(color = "black",size=0.5) +
+  theme_gray(8)+scale_fill_manual(values=c("#084594","#4292c6","#9ecae1","#deebf7"))+
+      theme(axis.text = element_text(size =10, color="black"),
+            axis.text.x = element_text(),
+          axis.title = element_text(size =15),
+          legend.text = element_text(size =14),
+          legend.title = element_text(size =12),
+          legend.position = c(1, 0.4), legend.justification = c(1,1),
+          plot.margin=unit(c(0,2.25,1,1),"cm"))+
+    xlab("Adjusted Principle Component")+ylab(NULL)
+  
+  grid.arrange(scree, heat, ncol=1, widths = c(4, 1), heights = c(2, 4))
+}
+
+
+
+heat_scree_plot(Loadings, Importance)
